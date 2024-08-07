1fb64b62cd58c718106aec1f59df4245655acacc
Daniel <dannyed784@gmail.com>
2024-08-06 21:41:19 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+//board
+let board;
+let boardWidth = 500;
+let boardHeight = 500;
+let context;
+
+//player
+let playerWidth = 80;
+let playerHeight = 10;
+let playerVelocityX = 10;
+
+let player = {
+  x: boardWidth / 2 - playerWidth / 2,
+  y: boardHeight - playerHeight - 5,
+  width: playerWidth,
+  height: playerHeight,
+  velocityX: playerVelocityX,
+};
+
+window.onload = function () {
+  board = document.getElementById('board');
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext('2d'); // used for drawing on the board.
+
+  //draw initial player
+  context.fillStyle = 'lightgreen';
+  context.fillRect(player.x, player.y, player.width, player.height);
+
+  requestAnimationFrame(update);
+  document.addEventListener('keydown', movePlayer);
+};
+
+function update() {
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
+
+  context.fillStyle = 'lightgreen';
+  context.fillRect(player.x, player.y, player.width, player.height);
+}
+
+function outOfBounds(xPosition) {
+  return xPosition < 0 || xPosition + playerWidth > boardWidth;
+}
+
+function movePlayer(e) {
+  if (e.code == 'ArrowLeft') {
+    // player.x -= player.velocityX;
+    let nextPlayerX = player.x - player.velocityX;
+    if (!outOfBounds(nextPlayerX)) {
+      player.x = nextPlayerX;
+    }
+  } else if (e.code == 'ArrowRight') {
+    // player.x += player.velocityX;
+    let nextPlayerX = player.x + player.velocityX;
+    if (!outOfBounds(nextPlayerX)) {
+      player.x = nextPlayerX;
+    }
+  }
+}
